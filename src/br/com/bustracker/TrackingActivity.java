4ce7d08ef508d3ff715e72110b8cdadf81d10c1c
Patrick Maia <pmaia@thoughtworks.com>
2013-12-05 15:36:36 -0300

    mostra as coordenadas

--- src/br/com/bustracker/TrackingActivity.java
+++ src/br/com/bustracker/TrackingActivity.java
@@ -1,22 +1,69 @@
 package br.com.bustracker;
 
-import android.os.Bundle;
+import java.util.Locale;
+
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.Toast;
 
-public class TrackingActivity extends Activity {
+public class TrackingActivity extends Activity implements LocationListener {
 
+	private LocationManager locationManager;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tracking);
+		// Get the location manager
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);		
+	}
+	
+	private void showLocation(Location location) {
+		if(location == null) {
+			return;
+		}
+		
+		String strLocation = String.format(Locale.getDefault(), "Lat.: %f Long.: %f", location.getLatitude(), location.getLongitude());
+		Context context = getApplicationContext();
+		Toast toast = Toast.makeText(context, strLocation, Toast.LENGTH_LONG);
+		toast.show();
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.tracking, menu);
-		return true;
+	protected void onResume() {
+		super.onResume();
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);		
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		locationManager.removeUpdates(this);
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		showLocation(location);
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
 	}
 
 }
